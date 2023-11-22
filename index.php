bbac69fee290f6eae2969389bcc0dfaba48f5c96
HIEUNMPH35961 <nguyenminhhieu06042004@gmail.com>
2023-11-23 00:02:10 +0700

    commit

--- index.php
+++ index.php
@@ -11,11 +11,22 @@ if (isset($_GET['act']) && ($_GET['act'] != "")) {
             $list_sanpham = loadAll_sanpham_home();
             include "view/thucdon.php";
             break;
+        case "danh_muc":
+            if (isset($_GET['id']) && ($_GET['id'] > 0)) {
+                $id = $_GET['id'];
+                $list_dm = loadAll_danhmuc();
+                $list_sanpham = load_sanpham_cungloai("", $id);
+                include "view/dm_thucdon.php";
+            } else {
+                $listsp = loadAll_sanpham("", 0);
+                include "view/danhmuc.php";
+            }
+            break;
         default:
             include "view/home.php";
             break;
     }
-}else{
+} else {
     include "view/home.php";
 }
 include "view/layout/footer.php";
